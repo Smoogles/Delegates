e89aa44cb59c02df362c8660b6a61ee4a318f932
james <chillin.jimmy99@gmail.com>
2022-07-04 16:29:59 +1000

    Start of delegates

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public delegate string WriteLog(string message);
+    public delegate void Del(string message);        
+        
+
+    private static void Main(string[] args)
+    {
+        Del handler = DelegateMethod;
+        handler("oof");
+        // Instantiate delegate variable log.
+        WriteLog log;
+        // Point to the method it uses.
+        log = ReturnMessage;
+        // Given method ReturnMessage returning message of 'Yeet'
+        var res = log("Yeet");
+        System.Console.WriteLine(res);
+    }
+    public static string ReturnMessage(string message)
+    {
+        return message;
+    }
+    public static void DelegateMethod(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
